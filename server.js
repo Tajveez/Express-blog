f64d646b2a9030608d5c3f6b903d02476ce326e7
Tajveez <tajveez786@gmail.com>
2020-08-22 18:04:21 +0500

    Code changes

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ const express = require("express");
 const articleRouter = require("./routes/articles");
 const mongoose = require("mongoose");
 const Article = require("./models/article");
+const methodOverride = require('method-override')
 const app = express();
 // https://youtu.be/1NrHkjlWVhM?t=237
 mongoose.connect("mongodb://localhost/blog", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true
 });
 
 app.set("view engine", "ejs");
-
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
 
 app.get("/", async (req, res) => {
   const articles = await Article.find().sort({ createdAt: "desc" });
